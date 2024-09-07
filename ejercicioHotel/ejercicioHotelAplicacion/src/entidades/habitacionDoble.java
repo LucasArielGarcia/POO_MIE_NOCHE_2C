1523464371a6686375459aefb97aee3d5f40ed44
LucasArielGarcia <85250463+LucasArielGarcia@users.noreply.github.com>
2024-09-07 17:06:25 -0300

    se sube parcialemente la aplicacion

---
+++ ejercicioHotel/ejercicioHotelAplicacion/src/entidades/habitacionDoble.java
@@ -0,0 +1,8 @@
+package entidades;
+
+public class habitacionDoble extends Habitacion{
+    @Override
+    protected double calcularprecioHabitacion() {
+        return 0;
+    }
+}
---
+++ ejercicioHotel/ejercicioHotelAplicacion/src/entidades/habitacionSimple.java
@@ -0,0 +1,8 @@
+package entidades;
+
+public class habitacionSimple extends Habitacion{
+    @Override
+    protected double calcularprecioHabitacion() {
+        return 0;
+    }
+}
